ff9fb1ac0305be6d7607351e0b1a58ca4d784f7a
Jacob Fromm <jacobfromm@gmail.com>
2020-08-03 19:29:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,7 +7,14 @@ def game_hash
       :players => [
         {
           :player_name => "",
-          :player_name => "",
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0,
           
         },  
         ]
